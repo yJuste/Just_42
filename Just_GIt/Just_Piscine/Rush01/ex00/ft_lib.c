6f4a563462a9dbdebb439e8c98fc89bbf9696d77
justJuste <jules.longin1@gmail.com>
2024-09-06 19:32:34 +0200

    modif DS_STore / add ft_lib

---
+++ Just_GIt/Just_Piscine/Rush01/ex00/ft_lib.c
@@ -0,0 +1,8 @@
+//
+//  ft_lib.c
+//  JustX_42
+//
+//  Created by Jules Longin on 05/09/2024.
+//
+
+#include "ft_lib.h"
---
+++ Just_GIt/Just_Piscine/Rush01/ex00/ft_lib.h
@@ -0,0 +1,13 @@
+//
+//  ft_lib.h
+//  JustX_42
+//
+//  Created by Jules Longin on 05/09/2024.
+//
+
+#ifndef ft_lib_h
+#define ft_lib_h
+
+#include <stdio.h>
+
+#endif /* ft_lib_h */
