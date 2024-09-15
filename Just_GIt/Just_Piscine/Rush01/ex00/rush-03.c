7c995564795aa8765e150c9b1212db9a16a84c2d
justJuste <jules.longin1@gmail.com>
2024-09-15 18:42:58 +0200

    add rush-03 in rush01

--- Just_GIt/Just_Piscine/Rush01/ex00/rush-03.c
+++ Just_GIt/Just_Piscine/Rush01/ex00/rush-03.c
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   rush-02.c                                          :+:      :+:    :+:   */
+/*   rush-03.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By:                                            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -11,93 +11,104 @@
 /* ************************************************************************** */
 #include "main.h"
 
-#define MAX_N 9 // Taille maximale de la grille
+void	ft_combination(int size);
+void	ft_fill_grid(int **grid, int row, int col, int *count, int size);
+int	ft_is_valid(int **grid, int row, int col, int num, int size);
+void	ft_print_grid(int **grid, int size);
+int	main(void);
 
-// Fonction pour afficher la grille
-void afficherGrille(int grid[MAX_N][MAX_N], int N) {
-    for (int i = 0; i < N; i++) {
-	for (int j = 0; j < N; j++) {
-	    printf("%d ", grid[i][j]);
+void	ft_print_grid(int **grid, int size)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < size)
+	{
+		j = 0;
+		while (j < size)
+		{
+			printf("%d ", grid[i][j]);
+			j++;
+		}
+		printf("\n");
+		i++;
 	}
-	printf("\n");
-    }
-    printf("-------------------------------\n"); // Séparateur entre les grilles
+	ft_putstr("-------------------------------\n");
 }
 
-// Fonction pour vérifier s'il est sûr de placer un nombre dans une cellule
-int estValide(int grid[MAX_N][MAX_N], int row, int col, int num, int N) {
-    // Vérifier si le nombre est déjà présent dans la ligne
-    for (int x = 0; x < N; x++) {
-	if (grid[row][x] == num) {
-	    return 0;
+int	ft_is_valid(int **grid, int row, int col, int num, int size)
+{
+	int	x;
+	
+	x = 0;
+	while (x < size)
+	{
+		if (grid[row][x] == num || grid[x][col] == num)
+			return (0);
+		x++;
 	}
-    }
-
-    // Vérifier si le nombre est déjà présent dans la colonne
-    for (int x = 0; x < N; x++) {
-	if (grid[x][col] == num) {
-	    return 0;
-	}
-    }
-
-    return 1; // Le nombre peut être placé dans la cellule
+	return (1);
 }
 
-// Fonction récursive pour remplir la grille et afficher toutes les solutions
-void remplirGrille(int grid[MAX_N][MAX_N], int row, int col, int* count, int N) {
-    // Si nous atteignons la fin de la grille, une solution est trouvée
-    if (row == N - 1 && col == N) {
-	(*count)++;  // Incrémenter le compteur de solutions
-	printf("Solution #%d:\n", *count);
-	afficherGrille(grid, N); // Afficher la grille
-	return;
-    }
+void	ft_fill_grid(int **grid, int row, int col, int *count, int size)
+{
+	int	num;
 
-    // Passer à la ligne suivante si nous atteignons la fin d'une ligne
-    if (col == N) {
-	row++;
-	col = 0;
-    }
-
-    // Essayer de placer les chiffres de 1 à N dans la cellule
-    for (int num = 1; num <= N; num++) {
-	if (estValide(grid, row, col, num, N)) {
-	    grid[row][col] = num; // Placer le chiffre
-
-	    // Passer à la cellule suivante
-	    remplirGrille(grid, row, col + 1, count, N);
-
-	    // Retirer le chiffre après avoir testé la solution
-	    grid[row][col] = 0;
+	if (row == size - 1 && col == size)
+	{
+		(*count)++;
+		return ;
 	}
-    }
+	if (col == size)
+	{
+		row++;
+		col = 0;
+	}
+	num = 1;
+	while (num <= size)
+	{
+		if (ft_is_valid(grid, row, col, num, size))
+		{
+			grid[row][col] = num;
+			ft_fill_grid(grid, row, col + 1, count, size);
+			grid[row][col] = 0;
+		}
+		num++;
+	}
 }
 
-// Fonction principale pour générer et afficher toutes les grilles de taille NxN
-void genererToutesLesGrilles(int N) {
-    int grid[MAX_N][MAX_N] = {0}; // Initialiser une grille vide
-    int count = 0;                // Compteur de solutions
+void	ft_combination(int size)
+{
+	int	**grid;
+	int	count;
+	int	i;
 
-    // Appel de la fonction récursive pour remplir la grille
-    remplirGrille(grid, 0, 0, &count, N);
-
-    // Afficher le nombre total de solutions
-    printf("Nombre total de solutions pour une grille %dx%d: %d\n", N, N, count);
+	count = 0;
+	i = 0;
+	grid = malloc(size * sizeof(int *));
+	while (i < size)
+	{
+		grid[i] = malloc(size * sizeof(int));
+		i++;
+	}
+	ft_fill_grid(grid, 0, 0, &count, size);
+	printf("Nombre total de solutions pour une grille %dx%d: %d\n", size, size, count);
+	while (i < size)
+	{
+		free(grid[i]);
+		i++;
+	}
+	free(grid);
+	return ;
 }
 
-int main() {
-    int N;
-
-    // Demander à l'utilisateur la taille de la grille
-    printf("Entrez la taille de la grille (entre 1 et 9): ");
-    scanf("%d", &N);
-
-    // Vérifier que la taille est valide
-    if (N >= 1 && N <= 9) {
-	genererToutesLesGrilles(N); // Générer les solutions pour une grille de taille NxN
-    } else {
-	printf("Taille invalide. Veuillez entrer un nombre entre 1 et 9.\n");
-    }
-
-    return 0;
+int	main(void)
+{
+	int	size;
+	
+	size = 4;
+	if (size >= 1 && size <= 9)
+		ft_combination(size);
+	return (0);
 }
