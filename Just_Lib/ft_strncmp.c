621aac62e6426eebe8df6db9c12f8deff382a8d5
justJuste <jules.longin1@gmail.com>
2024-10-01 17:02:32 +0200

    erreur ft_strncmp

--- Just_Lib/ft_strncmp.c
+++ Just_Lib/ft_strncmp.c
@@ -22,7 +22,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0' && i < n)
 		i++;
-	while (i == n)
+	if (i == n)
 		return (0);
 	return (s1[i] - s2[i]);
 }
