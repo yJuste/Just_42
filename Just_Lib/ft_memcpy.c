f6624d6cceec66cb0566115037e65923fb1ac86d
justJuste <jules.longin1@gmail.com>
2024-10-10 01:55:07 +0200

    add memcpy

---
+++ Just_Lib/ft_memcpy.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By:                                            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created:   by 42                                  #+#    #+#             */
+/*   Updated:   by 42                                 ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+/*   • Copies to n bytes a memory block.                                      */
+/*   • Prototype:   VOID* ( void *dst, void *src, size_t n)                   */
+/*        -> size_t                                                           */
+/* ************************************************************************** */
+#include <stdlib.h>
+
+// more commonly used with structures or arrays.
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t				i;
+	unsigned char		*dest;
+	const unsigned char	*source;
+
+	if (!dst || !src)
+		return (NULL);
+	dest = (unsigned char *)dst;
+	source = (const unsigned char *)src;
+	i = 0;
+	while (i < n)
+	{
+		dest[i] = source[i];
+		i++;
+	}
+	return (dst);
+}
